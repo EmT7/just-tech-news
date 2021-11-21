cbb6606e0d94173d5e1dfca5db6c3dc0b4ccaf3e
Miranda <88161424+MirandaT77@users.noreply.github.com>
2021-11-21 14:43:20 -0800

    ran snapshot

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -18,9 +18,12 @@ const sess = {
     db: sequelize
   })
 };
+
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -28,7 +31,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use(require('./controllers/'));
 
